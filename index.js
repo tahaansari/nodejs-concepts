95cce1224915dcc919f8c7d592828119027b2f56
Taha Ansari <taha.m.ansari@gmail.com>
2025-05-23 12:14:14 +0530

    wip

--- index.js
+++ index.js
@@ -69,25 +69,63 @@
 
 // DownloadFile().then(()=> console.log('file download completed')).catch(()=>console.log("failed to download file"))
 
+console.clear();
+
+// function sum(a,b){
+//     if(a+b == 3){
+//         throw new Error("please dont put 1 and 2"); 
+//     }else{
+//         return a+b;
+//     }
+    
+// }
+
+// try{
+//     console.log(sum(1,2));
+// }catch(err){
+//     console.log(`${err.message}`);
+// }
 
 function step1(callback){
-    //
+    let status = 200;
+    if(status == 200){
+        callback()
+    }else{
+        throw new Error('failed')
+    }
 }
-function step2(){
-    return "success"
+function step2(callback){
+    let status = 201;
+    if(status == 200){
+        callback()
+    }else{
+        throw new Error('failed')
+    }
 }
-function step3(){
-    return "success"
+
+function step3(callback){
+    let status = 200;
+    if(status == 200){
+        callback()
+    }else{
+        throw new Error('failed')
+    }
 }
 
 // CALLBACK HELL
-// step1(function(){
-//     step2(function(){
-//         step3(function(){
-//             console.log('all steps completed')
-//         })
-//     })
-// })
+
+try{
+    step1(function(){
+        step2(function(){
+            step3(function(){
+                console.log("All steps completed successfully")
+            })
+        })
+    })
+}catch(err){
+    console.log(`some error occured ${err}`)   
+}
+
 
 // PROMISE
 // step1().then(step2).then(step3).then(()=> console.log('all steps completed')).catch(()=>console.log('failed'))
